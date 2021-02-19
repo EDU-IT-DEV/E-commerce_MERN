017847b50eae5a412af328df982f7e085c894900
Eduardo Miguel Munoz <edu.programmer.it@gmail.com>
2021-02-19 13:08:53 -0300

    Displaying Products Pagination and Carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { getProductsByCount } from "../functions/product";
+import React from "react";
+import Jumbotron from "../components/cards/Jumbotron";
+import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    getProductsByCount(3).then((res) => {
-      setProducts(res.data);
-    });
-  };
-
   return (
-    <div>
-      <p>react home</p>
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className="jumbotron text-danger h1 font-weight-bold text-center">
+        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
+      </div>
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        New Arrivals
+      </h4>
+      <NewArrivals />
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Best Sellers
+      </h4>
+      <BestSellers />
+
+      <br />
+      <br />
+    </>
   );
 };
 
